1882a0a6e70a9c9aefa4bb9bc721f9ab27173674
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-02-28 12:30:09 -0500

    source files

---
+++ CloneCloneHero/Chart.cpp
@@ -0,0 +1,33 @@
+#include "Chart.h"
+
+Chart::Chart() {
+
+}
+
+Chart::Chart(int notes, int type, int delay, float sustain) {
+
+}
+
+Chart::~Chart() {
+
+}
+
+void Chart::addChord(int notes, int type, float delay, float sustain) {
+
+}
+
+void Chart::copyBeat(int index) {
+
+}
+
+void Chart::copyMeasure(int index) {
+
+}
+
+void Chart::copyPart(float begin, float end) {
+
+}
+
+string Chart::toString() {
+
+}
---
+++ CloneCloneHero/CloneCloneHero.cpp
@@ -0,0 +1,13 @@
+#include "Game.h"
+
+using namespace std;
+
+/**
+ ** Auteur: Alex Lajeunesse
+ **/
+int main()
+{
+	Game game;
+
+	return 0;
+}
