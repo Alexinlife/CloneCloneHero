04b3642b2cdd4be9305258c60f55393973ba05b3
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-03-01 11:12:14 -0500

    refactor Game to Editor

---
+++ CloneCloneHero/Editor.h
@@ -0,0 +1,35 @@
+#include "Chart.h"
+
+#ifndef EDITOR_H
+#define EDITOR_H
+
+/**
+* Author: Alex Lajeunesse
+* Date: 27-02-2022
+*
+* Class: Editor
+* Description: Creates charts to be used by the game
+**/
+class Editor
+{
+public:
+	/**
+	* Default constructor for Editor
+	*/
+	Editor();
+
+	/**
+	* Destructor for Editor
+	*/
+	~Editor();
+
+	/**
+	* Starts the Editor
+	*/
+	void play();
+
+private:
+	Chart chart;
+};
+
+#endif
